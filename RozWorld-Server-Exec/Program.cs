76fcb1c940f33a7e851df176a9e9d72e9d97c970
rozniak <roryfewell@hotmail.com>
2016-06-16 12:33:22 +0100

    Adding CLI support, cmds work :+1:
    Even with some nice updating in the logger! :100:

--- RozWorld-Server-Exec/Program.cs
+++ RozWorld-Server-Exec/Program.cs
@@ -7,13 +7,61 @@ namespace Oddmatics.RozWorld.ServerExecutive
 {
     class Program
     {
+        public const int CharLimit = 100;
+        public static string CliInput { get; private set; }
+
         static void Main(string[] args)
         {
             var server = new RwServer();
+            bool shouldClose = false;
+
+            CliInput = String.Empty;
             server.Logger = new ConsoleLogger();
+            server.Stopping += delegate(object sender, EventArgs e)
+                                { shouldClose = true; };
             RwCore.SetServer(server);
             server.Start();
-            Console.ReadKey(true);
+
+            while (!shouldClose)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyPressed = Console.ReadKey();
+
+                    switch (keyPressed.KeyChar)
+                    {
+                            // Enter key
+                        case '\r':
+                            // Reason for this first bit is so that the Logger moves on the next line
+                            // and doesn't bother rewriting the command (want it to remain in the Logger)
+                            string cmd = CliInput;
+                            CliInput = String.Empty;
+                            Console.WriteLine();
+
+                            if (cmd.StartsWith("/") && cmd.Length > 1)
+                                server.SendCommand(server.ServerAccount, cmd.Substring(1));
+                            else
+                                server.Logger.Out("[ERR] Unknown command.");
+
+                            break;
+
+                            // Backspace
+                        case '\b':
+                            Console.Write(" ");
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+
+                            if (CliInput.Length > 0)
+                                CliInput = CliInput.Substring(0, CliInput.Length - 1);
+                            break;
+
+                            // Any other key and backspace
+                        default:
+                            if (CliInput.Length < Program.CharLimit)
+                                CliInput += keyPressed.KeyChar;
+                            break;
+                    }
+                }
+            }
         }
     }
 
@@ -27,7 +75,16 @@ namespace Oddmatics.RozWorld.ServerExecutive
 
         public void Out(string message)
         {
+            if (!string.IsNullOrEmpty(Program.CliInput))
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                for (int i = 0; i < Program.CharLimit; i++)
+                    Console.Write(" ");
+                Console.SetCursorPosition(0, Console.CursorTop);
+            }
+                
             Console.WriteLine("[" + DateTime.Now.ToString() + "] " + message);
+            Console.Write(Program.CliInput);
         }
 
         public bool Save()
